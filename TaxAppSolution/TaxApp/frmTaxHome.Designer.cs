fa1cd1a603f76792a642b4e7b2b0be908213ee37
Joshua Mckenna <36484731+PopGTN@users.noreply.github.com>
2023-09-15 10:55:52 -0300

    ewawe

---
+++ TaxAppSolution/TaxApp/frmTaxHome.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace TaxApp
+{
+    partial class frmTaxHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtIncome = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblIncome = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lstDisplay = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(187, 28);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtIncome
+            // 
+            this.txtIncome.Location = new System.Drawing.Point(187, 61);
+            this.txtIncome.Name = "txtIncome";
+            this.txtIncome.Size = new System.Drawing.Size(100, 20);
+            this.txtIncome.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(113, 28);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(38, 13);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name:";
+            // 
+            // lblIncome
+            // 
+            this.lblIncome.AutoSize = true;
+            this.lblIncome.Location = new System.Drawing.Point(113, 64);
+            this.lblIncome.Name = "lblIncome";
+            this.lblIncome.Size = new System.Drawing.Size(45, 13);
+            this.lblIncome.TabIndex = 3;
+            this.lblIncome.Text = "Income:";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(116, 87);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(223, 87);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lstDisplay
+            // 
+            this.lstDisplay.FormattingEnabled = true;
+            this.lstDisplay.Location = new System.Drawing.Point(12, 157);
+            this.lstDisplay.Name = "lstDisplay";
+            this.lstDisplay.Size = new System.Drawing.Size(450, 95);
+            this.lstDisplay.TabIndex = 6;
+            this.lstDisplay.TabStop = false;
+            // 
+            // frmTaxHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(479, 276);
+            this.Controls.Add(this.lstDisplay);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblIncome);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtIncome);
+            this.Controls.Add(this.txtName);
+            this.Name = "frmTaxHome";
+            this.Text = "Tax App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtIncome;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblIncome;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ListBox lstDisplay;
+    }
+}
+
---
+++ TaxAppSolution/TaxApp/frmTaxHome.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ * Author: Donnie McKinnon
+ * Date: Sep 13/23
+ * Class: CIS-2225
+ * Description: App that accepts name, income and calculates tax amount to be deducted.
+ */
+namespace TaxApp
+{
+    public partial class frmTaxHome : Form
+    {
+        //Globals used to keep running totals.
+        double gTotalTax = 0.0;
+        double gTotalIncome = 0.0;
+        public frmTaxHome()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            double taxAmount = 0.0;
+            //use a tryParse to see if value in txtIncome can be converted to a double
+            if (double.TryParse(txtIncome.Text, out double income) == false)
+            {
+                MessageBox.Show("Please enter a numeric value for income. ");
+                txtIncome.Focus();
+            }
+            else
+            {
+                taxAmount = calcTax(income);
+                //MessageBox.Show("The tax amount: " + taxAmount);//used for debugging
+                string outPut = txtName.Text + " total income: " + income 
+                    + " Total taxes pay: " + taxAmount + ". Total after taxes: "+ 
+                    (income - taxAmount);
+                lstDisplay.Items.Add(outPut);
+                txtName.Text = "";
+                txtIncome.Text = "";
+                gTotalTax += taxAmount;
+                gTotalIncome += income;
+
+            }
+        }
+        
+        private double calcTax(double amt)
+        {
+            double returnValue = 0.0;
+            if (amt > 50000)
+            {
+                returnValue = amt * 0.2;
+            }
+            else if (amt > 25000)
+            {
+                returnValue = amt * 0.15;
+            }
+            else
+            {
+                returnValue = amt * 0.1;
+            }
+            return returnValue;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Total income: " + gTotalIncome + " total taxes: " + gTotalTax);
+            lstDisplay.Items.Clear();
+            txtIncome.Text = "";
+            txtName.Text = "";
+            gTotalIncome = 0.0;
+            gTotalTax = 0.0;
+        }
+    }
+}
