3fed1334c2e6aeb06d9b85defadf1cb7bf6b00a0
Joshua Mckenna <36484731+PopGTN@users.noreply.github.com>
2023-09-15 11:35:08 -0300

    ok

---
+++ CIS2225Assignment1/CIS2225Assignment1/frmMain.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace CIS2225Assignment1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.TxtGrade = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblCourseName = new System.Windows.Forms.Label();
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lstOuputBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(109, 290);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(75, 23);
+            this.btnAverage.TabIndex = 0;
+            this.btnAverage.Text = "Average";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(28, 290);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // TxtGrade
+            // 
+            this.TxtGrade.Location = new System.Drawing.Point(105, 77);
+            this.TxtGrade.Name = "TxtGrade";
+            this.TxtGrade.Size = new System.Drawing.Size(79, 20);
+            this.TxtGrade.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(105, 51);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(79, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // lblCourseName
+            // 
+            this.lblCourseName.AutoSize = true;
+            this.lblCourseName.Location = new System.Drawing.Point(25, 54);
+            this.lblCourseName.Name = "lblCourseName";
+            this.lblCourseName.Size = new System.Drawing.Size(74, 13);
+            this.lblCourseName.TabIndex = 5;
+            this.lblCourseName.Text = "Course Name:";
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.AutoSize = true;
+            this.lblGrade.Location = new System.Drawing.Point(25, 80);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(39, 13);
+            this.lblGrade.TabIndex = 6;
+            this.lblGrade.Text = "Grade:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(63, 319);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // lstOuputBox
+            // 
+            this.lstOuputBox.FormattingEnabled = true;
+            this.lstOuputBox.Location = new System.Drawing.Point(28, 150);
+            this.lstOuputBox.Name = "lstOuputBox";
+            this.lstOuputBox.Size = new System.Drawing.Size(156, 134);
+            this.lstOuputBox.TabIndex = 8;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(218, 400);
+            this.Controls.Add(this.lstOuputBox);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblGrade);
+            this.Controls.Add(this.lblCourseName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.TxtGrade);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnAverage);
+            this.Name = "frmMain";
+            this.Text = "Grades";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox TxtGrade;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblCourseName;
+        private System.Windows.Forms.Label lblGrade;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ListBox lstOuputBox;
+    }
+}
+
--- CIS2225Assignment1/CIS2225Assignment1/frmMain.cs
+++ CIS2225Assignment1/CIS2225Assignment1/frmMain.cs
@@ -10,11 +10,13 @@ using System.Windows.Forms;
 
 namespace CIS2225Assignment1
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
         }
+
+
     }
 }
